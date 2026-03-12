5780e5de407365e1f287c4c9fcb1b472aedb250a
Roberth <mroberth.alvarez@gmail.com>
2026-03-12 19:15:16 -0400

    Nueva fuente agregada: Inter

--- app/Views/template/head.php
+++ app/Views/template/head.php
@@ -7,9 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="<?= BASE_URL ?>dist/img/dirpoles.ico" type="image/x-icon">
   <title>DIRPOLES 4 - <?= $titulo ?></title>
-  <link
-    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-    rel="stylesheet">
+  <link href="<?= BASE_URL ?>plugins/fonts/inter/inter.css" rel="stylesheet">
   <link rel="stylesheet" href="<?= BASE_URL ?>plugins/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?= BASE_URL ?>plugins/fontawesome/css/all.min.css">
   <link rel="stylesheet" href="<?= BASE_URL ?>plugins/sweetalert2/dist/sweetalert2.min.css">
