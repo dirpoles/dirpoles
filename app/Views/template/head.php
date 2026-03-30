af1330de13b77e28354e8b6d59a9f461a0c8f77e
mroberth <mroberth.alvarez@gmail.com>
2026-03-30 13:01:51 -0400

    Ajustes de rendimiento realizados mediante Google LightHouse

--- app/Views/template/head.php
+++ app/Views/template/head.php
@@ -7,15 +7,45 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="<?= BASE_URL ?>dist/img/dirpoles.ico" type="image/x-icon">
   <title>DIRPOLES 4 - <?= $titulo ?></title>
+
+  <!-- Pre-carga de recursos críticos (Fuentes, CSS, Imagen LCP) -->
+  <link rel="preload" href="<?= BASE_URL ?>plugins/fonts/inter/files/inter-400.ttf" as="font" type="font/ttf" crossorigin>
+  <link rel="preload" href="<?= BASE_URL ?>plugins/fonts/inter/files/inter-600.ttf" as="font" type="font/ttf" crossorigin>
+  <link rel="preload" href="<?= BASE_URL ?>plugins/bootstrap/dist/css/bootstrap.min.css" as="style">
+  
+  <?php if (isset($titulo) && $titulo === "Login"): ?>
+    <link rel="preload" href="<?= BASE_URL ?>dist/img/fondo.jpg" as="image">
+    <link rel="preload" href="<?= BASE_URL ?>dist/css/etc/login.css" as="style">
+    <!-- Estilos Críticos en línea para evitar FOUC y bloqueo -->
+    <style>
+      :root{--dirpoles-blue:#004a99;--dirpoles-blue-dark:#003366;}
+      body,html{height:100%;margin:0;font-family:'Inter',sans-serif;}
+      .login-page{display:flex;min-height:100vh;}
+      .login-left{background:#fff;z-index:10;}
+      .login-right{flex:1;background-color:#f0f2f5;display:none;}
+      @media(min-width:768px){.login-right{display:block;}}
+      .bg-image{height:100%;background-size:cover;background-position:center;}
+    </style>
+  <?php endif; ?>
+
   <link href="<?= BASE_URL ?>plugins/fonts/inter/inter.css" rel="stylesheet">
   <link rel="stylesheet" href="<?= BASE_URL ?>plugins/bootstrap/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="<?= BASE_URL ?>plugins/fontawesome/css/all.min.css">
-  <link rel="stylesheet" href="<?= BASE_URL ?>plugins/sweetalert2/dist/sweetalert2.min.css">
-  <link rel="stylesheet" href="<?= BASE_URL ?>plugins/select2/css/select2.min.css">
-  <link rel="stylesheet" href="<?= BASE_URL ?>plugins/DataTables/css/datatables.min.css">
-  <link rel="stylesheet" href="<?= BASE_URL ?>dist/css/select-2-bootstrap-5/select2-bootstrap-5-theme.min.css">
-  <link href="<?= BASE_URL ?>dist/css/dashboard/sb-admin-2.min.css" rel="stylesheet">
-  <link href="<?= BASE_URL ?>dist/css/etc/sidebar.css" rel="stylesheet">
-  <link href="<?= BASE_URL ?>plugins/driver.js/driver.css" rel="stylesheet">
-  <link href="<?= BASE_URL ?>dist/css/driver.js-personalizado/temaDriverPersonalizado.css" rel="stylesheet">
+  
+  <!-- Solo cargar librerías pesadas si no estamos en el Login -->
+  <?php if (!isset($titulo) || $titulo !== "Login"): ?>
+    <link rel="stylesheet" href="<?= BASE_URL ?>plugins/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>plugins/select2/css/select2.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>plugins/DataTables/css/datatables.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>dist/css/select-2-bootstrap-5/select2-bootstrap-5-theme.min.css">
+    <link href="<?= BASE_URL ?>dist/css/dashboard/sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?= BASE_URL ?>dist/css/etc/sidebar.css" rel="stylesheet">
+    <link href="<?= BASE_URL ?>plugins/driver.js/driver.css" rel="stylesheet">
+    <link href="<?= BASE_URL ?>dist/css/driver.js-personalizado/temaDriverPersonalizado.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?= BASE_URL ?>plugins/sweetalert2/dist/sweetalert2.min.css">
+  <?php else: ?>
+    <!-- En Login: Carga Asíncrona de estilos no críticos -->
+    <link rel="stylesheet" href="<?= BASE_URL ?>plugins/fontawesome/css/all.min.css" media="print" onload="this.media='all'">
+    <link rel="stylesheet" href="<?= BASE_URL ?>plugins/sweetalert2/dist/sweetalert2.min.css" media="print" onload="this.media='all'">
+    <link rel="stylesheet" href="<?= BASE_URL ?>dist/css/etc/login.css">
+  <?php endif; ?>
 </head>
--- app/Views/template/script.php
+++ app/Views/template/script.php
@@ -1,45 +1,53 @@
 <!-- jQuery primero (importante para Bootstrap) -->
 <script src="<?= BASE_URL ?>plugins/jquery/jquery.min.js"></script>
 
-<!-- Bootstrap -->
-<script src="<?= BASE_URL ?>plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Bootstrap con defer para no bloquear el renderizado -->
+<script src="<?= BASE_URL ?>plugins/bootstrap/dist/js/bootstrap.bundle.min.js" defer></script>
 
-<!-- FullCalendar CORE primero -->
-<script src="<?= BASE_URL ?>plugins/@fullcalendar/core/index.global.min.js"></script>
-<script src="<?= BASE_URL ?>plugins/@fullcalendar/core/locales/es.global.min.js"></script>
+<!-- Carga selectiva de librerías pesadas -->
+<?php if (!isset($titulo) || $titulo !== "Login"): ?>
+    <!-- FullCalendar CORE -->
+    <script src="<?= BASE_URL ?>plugins/@fullcalendar/core/index.global.min.js" defer></script>
+    <script src="<?= BASE_URL ?>plugins/@fullcalendar/core/locales/es.global.min.js" defer></script>
 
-<!-- FullCalendar PLUGINS después -->
-<script src="<?= BASE_URL ?>plugins/@fullcalendar/bootstrap5/index.global.min.js"></script>
-<script src="<?= BASE_URL ?>plugins/@fullcalendar/daygrid/index.global.min.js"></script>
-<script src="<?= BASE_URL ?>plugins/@fullcalendar/interaction/index.global.min.js"></script>
-<script src="<?= BASE_URL ?>plugins/@fullcalendar/timegrid/index.global.min.js"></script>
-<script src="<?= BASE_URL ?>plugins/@fullcalendar/list/index.global.min.js"></script>
+    <!-- FullCalendar PLUGINS -->
+    <script src="<?= BASE_URL ?>plugins/@fullcalendar/bootstrap5/index.global.min.js" defer></script>
+    <script src="<?= BASE_URL ?>plugins/@fullcalendar/daygrid/index.global.min.js" defer></script>
+    <script src="<?= BASE_URL ?>plugins/@fullcalendar/interaction/index.global.min.js" defer></script>
+    <script src="<?= BASE_URL ?>plugins/@fullcalendar/timegrid/index.global.min.js" defer></script>
+    <script src="<?= BASE_URL ?>plugins/@fullcalendar/list/index.global.min.js" defer></script>
 
-<!-- Otros plugins -->
-<script src="<?= BASE_URL ?>plugins/fontawesome/js/all.min.js"></script>
-<script src="<?= BASE_URL ?>plugins/sweetalert2/dist/sweetalert2.min.js"></script>
-<script src="<?= BASE_URL ?>plugins/select2/js/select2.min.js"></script>
-<script src="<?= BASE_URL ?>plugins/DataTables/js/datatables.min.js"></script>
-<script src="<?= BASE_URL ?>plugins/moment/moment.min.js"></script>
-<script src="<?= BASE_URL ?>plugins/jspdf/jspdf.umd.min.js"></script>
-<script src="<?= BASE_URL ?>plugins/html2canvas/html2canvas.min.js"></script>
+    <!-- Otros plugins pesados -->
+    <script src="<?= BASE_URL ?>plugins/select2/js/select2.min.js" defer></script>
+    <script src="<?= BASE_URL ?>plugins/DataTables/js/datatables.min.js" defer></script>
+    <script src="<?= BASE_URL ?>plugins/moment/moment.min.js" defer></script>
+    <script src="<?= BASE_URL ?>plugins/jspdf/jspdf.umd.min.js" defer></script>
+    <script src="<?= BASE_URL ?>plugins/html2canvas/html2canvas.min.js" defer></script>
 
-<!-- Tus scripts -->
-<script src="<?= BASE_URL ?>dist/js/dashboard/sb-admin5.js"></script>
-<script src="<?= BASE_URL ?>dist/js/core/AlertManager.js"></script>
-<script src="<?= BASE_URL ?>dist/js/core/logout.js"></script>
-<script src="<?= BASE_URL ?>dist/js/dashboard/sidebar-active.js"></script>
-<script src="<?= BASE_URL ?>dist/js/dashboard/sidebar-scroll-enhancement.js"></script>
-<script src="<?= BASE_URL ?>dist/js/core/select-2-init.js"></script>
-<script src="<?= BASE_URL ?>dist/js/modulos/notificaciones/control.js"></script>
-<script src="<?= BASE_URL ?>dist/js/perfil/verPerfil.js"></script>
-<script src="<?= BASE_URL ?>dist/js/perfil/editarPerfil.js"></script>
-<script src="<?= BASE_URL ?>dist/js/core/PDFCustomizer.js"></script>
+    <!-- Scripts de la interfaz interna -->
+    <script src="<?= BASE_URL ?>dist/js/dashboard/sb-admin5.js" defer></script>
+    <script src="<?= BASE_URL ?>dist/js/dashboard/sidebar-active.js" defer></script>
+    <script src="<?= BASE_URL ?>dist/js/dashboard/sidebar-scroll-enhancement.js" defer></script>
+    <script src="<?= BASE_URL ?>dist/js/core/select-2-init.js" defer></script>
+    <script src="<?= BASE_URL ?>dist/js/modulos/notificaciones/control.js" defer></script>
+    <script src="<?= BASE_URL ?>dist/js/perfil/verPerfil.js" defer></script>
+    <script src="<?= BASE_URL ?>dist/js/perfil/editarPerfil.js" defer></script>
+    <script src="<?= BASE_URL ?>dist/js/core/PDFCustomizer.js" defer></script>
+    
+    <!-- Driver.js -->
+    <script src="<?= BASE_URL ?>plugins/driver.js/driver.js.iife.js" defer></script>
+<?php endif; ?>
+
+<!-- Plugins esenciales para el Login y alertas -->
+<script src="<?= BASE_URL ?>plugins/fontawesome/js/all.min.js" defer></script>
+<script src="<?= BASE_URL ?>plugins/sweetalert2/dist/sweetalert2.min.js" defer></script>
+<script src="<?= BASE_URL ?>dist/js/core/AlertManager.js" defer></script>
+<script src="<?= BASE_URL ?>dist/js/core/logout.js" defer></script>
+
 <script>
-    $(document).ready(function() {
-        $('[data-toggle="tooltip"]').tooltip();
+    document.addEventListener('DOMContentLoaded', function() {
+        if (typeof $ !== 'undefined' && $.fn.tooltip) {
+            $('[data-toggle="tooltip"]').tooltip();
+        }
     });
-</script>
-
-<!-- Driver.js -->
-<script src="<?= BASE_URL ?>plugins/driver.js/driver.js.iife.js"></script>
+</script>
